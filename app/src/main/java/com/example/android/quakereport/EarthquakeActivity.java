1ed935a632aa9541bbb4152c3a6ce3da798cdb44
Rachael Hall <rachael.hall.889@gmail.com>
2018-07-09 17:03:56 +0100

    networking as per lesson 5.15

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -29,34 +30,77 @@ import android.widget.AdapterView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
-    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+
+    private QuakeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        // Start the AsyncTask to fetch the earthquake data
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
 
+        // Find a reference to the {@link ListView} in the layout
         ListView quakeListView = (ListView) findViewById(R.id.list);
 
-        final QuakeAdapter adapter = new QuakeAdapter(this, earthquakes);
+        // create a new adapter that takes an empty list of earthquakes as an input
+       mAdapter = new QuakeAdapter(this, new ArrayList<Earthquake>());
 
-        quakeListView.setAdapter(adapter);
+       // set the adapter on the {@link ListView}
+        // so the list can be populated with the user interface
+        quakeListView.setAdapter(mAdapter);
 
+// set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake
         quakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Earthquake currentEarthquake = adapter.getItem(position);
+                // find the earthquake that was clicked on
+                Earthquake currentEarthquake = mAdapter.getItem(position);
+                // convert the striong url into a URI object to pas to the intent constructor
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                // create a new intent to view the earthquake URI
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                // send the intent to launch a new activity
                 startActivity(websiteIntent);
             }
         });
 
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+ // don't perform the request if there are no URLs or the first url is null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> data) {
+            // clear the adapter of previous earthquake data
+            mAdapter.clear();
+
+            // if there's a valid list of {@link Earthquake}s, then add them to the adapter's
+            // data set. This will triggfer the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+
+        }
     }
 }
